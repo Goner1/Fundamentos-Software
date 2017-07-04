231c51235584cf65f6806c0a6a4b065f0b83d702
OmarL <omarjesusleivasanchez18@gmail.com>
2017-07-03 20:03:56 -0400

    Add files via upload

---
+++ Notas de u alumno(Objeto)/Problema1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author omar
+ */
+public class Problema1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+       int a=1, b =2, c = 3, cont=0;
+       int[] correctas = new int[10];
+        
+        Nota notas = new Nota(correctas, cont, a, b, c);
+        notas.altercorrectas();
+        notas.respcorrectas();
+        notas.resultado();
+        
+        
+    }
+
+
+}
